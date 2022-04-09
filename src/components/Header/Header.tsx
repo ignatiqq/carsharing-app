5bf033fcf1cf72144d67ee549d626895aceb7829
Ignat <zzzcap3@gmail.com>
2022-04-09 23:28:59 +0300

    clear header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from "./Header.module.css";
-import { useAppSelector } from 'store/hooks';
 import {CitySelector} from "components";
 
 const Header = () => {
-  
-  const { currentCity } = useAppSelector(({location}) => ({
-    currentCity: location.cities.current
-  }))
-
-  console.log(currentCity)
 
   return (
     <header className={styles.header}>
