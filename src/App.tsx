0dd722b6a4ad4fecb8dd46faf7649e16773125a0
Ignat <zzzcap3@gmail.com>
2022-04-22 21:28:52 +0300

    car grid fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { Sidebar } from "components"
 import AppRoutes from "routes/AppRoutes";
 
 import styles from "./App.module.css";
-import { useAppDispatch } from "store/hooks";
-import { getAllCities } from "store/location/cities/actions";
 
 function App() {
   
