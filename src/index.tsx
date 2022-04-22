73752361c5ef35c04e24b4275d462cfc16a0c3e0
Ignat <zzzcap3@gmail.com>
2022-04-22 21:37:19 +0300

    browser router to has router migration

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux"
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import store from "store/store";
 import 'assets/styles/index.css';
